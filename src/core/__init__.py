525bf35f648e0cb8a383d17d8282918f034ccfdc
ermakov12 <ermak0618@gmail.com>
2026-02-26 23:52:21 +0300

    Sprint 1: Cleaned project skeleton, removed guilib, added __init__.py

--- src/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Core module for business logic and cryptography."""
-from .config import Config
-from .events import EventBus
-from .state_manager import StateManager
-from .key_manager import KeyManager
-
-__all__ = ['Config', 'EventBus', 'StateManager', 'KeyManager']
--- src/database/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Database module for SQLite operations."""
-from .db import DatabaseHelper
-from .models import VaultEntry, AuditLog, Setting, KeyStore
-
-__all__ = ['DatabaseHelper', 'VaultEntry', 'AuditLog', 'Setting', 'KeyStore']
