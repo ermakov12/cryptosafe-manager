d67c9bc0835a24cc2bcaba45bc1ac053673d1e27
ermakov12 <89580646+ermakov12@users.noreply.github.com>
2026-02-27 06:36:27 +0300

    Update test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,21 +1,22 @@
-import pytest
+# tests/test_database.py
 import sqlite3
-from src.database import db, models
+import pytest
+from src.database import models, db
 
 def test_db_connection(temp_db_path):
     database = db.Database(temp_db_path)
-    conn = database.get_connection()
-    assert isinstance(conn, sqlite3.Connection)
+    assert database.conn is not None
 
 def test_models_tables(temp_db_path):
     conn = sqlite3.connect(temp_db_path)
-    cursor = conn.cursor()
-    # Создаём таблицы
     models.create_tables(conn)
+    cursor = conn.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     tables = [r[0] for r in cursor.fetchall()]
     assert 'vault_entries' in tables
     assert 'audit_log' in tables
     assert 'settings' in tables
     assert 'key_store' in tables
+    conn.close()
+
 
