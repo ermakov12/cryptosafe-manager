f54b164e694d40cec60b6cec98755b71d546f18b
ermakov12 <89580646+ermakov12@users.noreply.github.com>
2026-02-27 06:43:40 +0300

    Update test_events.py

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,17 +1,5 @@
-# tests/test_events.py
 import pytest
-from src.core.events import EventBus
 from src.core.audit_logger import AuditLogger
-
-def test_event_publish_subscribe(event_bus):
-    received = []
-
-    def handler(data):
-        received.append(data)
-
-    event_bus.subscribe("TestEvent", handler)
-    event_bus.publish("TestEvent", {"foo": "bar"})
-    assert received[0] == {"foo": "bar"}
 
 def test_audit_logger(event_bus):
     logger = AuditLogger(event_bus)
