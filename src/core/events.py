6efdd6aa3ec1dd23b5490ecc329dc4b701795d83
ermakov12 <89580646+ermakov12@users.noreply.github.com>
2026-02-27 06:01:54 +0300

    Update events.py

--- src/core/events.py
+++ src/core/events.py
@@ -1,12 +1,20 @@
 from collections import defaultdict
+from typing import Callable, Dict, List, Any
+from enum import Enum
+
+
+class EventType(str, Enum):
+    ENTRY_CREATED = "entry_created"
+    ENTRY_DELETED = "entry_deleted"
+
 
 class EventBus:
     def __init__(self):
-        self.subscribers = defaultdict(list)
+        self._subscribers: Dict[EventType, List[Callable]] = defaultdict(list)
 
-    def subscribe(self, event_name, callback):
-        self.subscribers[event_name].append(callback)
+    def subscribe(self, event: EventType, handler: Callable):
+        self._subscribers[event].append(handler)
 
-    def publish(self, event_name, **kwargs):
-        for cb in self.subscribers[event_name]:
-            cb(**kwargs)
+    def publish(self, event: EventType, data: Any = None):
+        for handler in self._subscribers[event]:
+            handler(data)
